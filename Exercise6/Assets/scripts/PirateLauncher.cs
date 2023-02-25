8e1d87963b9ac8156ceb6946c0e12c6752715f61
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-25 19:56:58 +0000

    Defined settings for pirate items

---
+++ Exercise6/Assets/scripts/PirateLauncher.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateLauncher : Launcher
+{
+    // Start is called before the first frame update
+    override protected void Start()
+    {
+        cooldownSeconds = 1f;
+        base.Start();
+    }
+}
